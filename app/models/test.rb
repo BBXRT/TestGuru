3ed14289902fe1d5d08107469d810803e1732a61
Ivan_Ivanovich_Ivanov <antirsi2@yandex.ru>
2019-05-08 23:12:20 +0300

    lesson5

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,10 +2,10 @@ class Test < ApplicationRecord
 
   belongs_to :category
   has_many :passed_tests
+  has_many :questions
 
   def self.find_testnames_by_category(category)
     Test.joins(:category).where(categories: {title: category}).order(title: :desc).pluck(:title)
   end
 
 end
-
