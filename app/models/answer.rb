03670a8229e564e77c3a4ec0cc43028db44bb819
Ivan_Ivanovich_Ivanov <antirsi2@yandex.ru>
2019-05-11 21:55:58 +0300

    fix1

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,12 +1,14 @@
 class Answer < ApplicationRecord
+
   belongs_to :question
+
+  scope :correct, -> {where(correct: true)}
 
   validates :body, presence: true
   validate :validate_answer_count
 
-  scope :select_right_answers, -> {where(correct: true)}
-
   def validate_answer_count
-    errors.add(:body) if Answer.where(question_id: self.question_id).pluck(:body).size > 3
+    errors.add(:body) if question.answers.count > 3
   end
+
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,14 +5,18 @@ class Test < ApplicationRecord
   has_many :passed_tests
   has_many :questions
 
-  validates :title, presence: true,
-                    uniqueness: true
-
-  validates :level, numericality: { only_integer: true, greater_than: 0 }
-
   scope :select_simple_tests, -> {where(:level => (0..1))}
   scope :select_middle_tests, -> {where(:level => (2..4))}
   scope :select_hard_tests, -> {where(:level => (5..Float::INFINITY))}
-  scope :find_testnames_by_category, -> (category) {Test.joins(:category).where(categories: {title: category}).order(title: :desc).pluck(:title)}
+
+  validates :title, presence: true,
+                    uniqueness: true
+  validates :level, numericality: { only_integer: true, greater_than: 0 }
+
+
+
+  def self.find_testnames_by_category(category)
+    Test.joins(:category).where(categories: {title: category}).order(title: :desc).pluck(:title)
+  end
 
 end
