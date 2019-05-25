f948ed4ee1d699ce56d8967addfdebad1ab972e5
Ivan_Ivanovich_Ivanov <antirsi2@yandex.ru>
2019-05-25 22:12:28 +0300

    fix5

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
 
-  before_action :select_test_id
+  before_action :select_test_id, only: [:create]
 
   def index
     @test = Test.find(params[:test_id].to_i)
@@ -19,7 +19,7 @@ class QuestionsController < ApplicationController
   def create
     body = params[:question][:body]
     Question.create(body: body, test_id: @test.id)
-    render plain: 'Вопрос создан'
+    render inline: 'Вопрос создан <%= button_to "Назад к списку вопросов", test_questions_path, method: :get %>'
   end
 
   def destroy
