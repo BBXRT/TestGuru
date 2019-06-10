a3e18abe3ecc22eecdea8af59f1deaf2862b84f0
Ivan_Ivanovich_Ivanov <antirsi2@yandex.ru>
2019-06-11 01:05:00 +0300

    fix6

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,19 +17,20 @@ class QuestionsController < ApplicationController
 
   def create
     body = params[:question][:body]
+    @test = Test.find(params[:test_id])
     Question.create(body: body, test_id: @test.id)
-    redirect_to test_questions_path
+
+    redirect_to test_questions_path(@test)
   end
 
   def destroy
     @question = Question.find(params[:id])
     @question.destroy
-    redirect_to test_questions
   end
 
   def new
-    @test = Test.new
-    redirect_to index
+    @test = Test.find(params[:test_id])
+    @question = Question.new(test: @test)
   end
 
   def select_test_id
@@ -37,7 +38,16 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    #render plain: '23'
+    @question = Question.find(params[:id])
+    @test = @question.test
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.body = params['question']['body']
+
+    @question.save
+    redirect_to test_questions_path(@question.test)
   end
 
 end
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -5,13 +5,21 @@ module TestsHelper
   end
 
   def test_title
-    Test.find(params[:test_id]).title
+    Test.find(@test.id).title
   end
 
   def github_url
     title = 'Учебный проект в онлайн-школе Thinknetica'
     link = 'http://thinknetica.com/'
     link_to title, link, target: :_blank
+  end
+
+  def question_header
+    if params[:action] == 'edit'
+      "Редактирование вопроса (#{test_title})"
+    else
+      "Новый вопрос (для #{test_title}):"
+    end
   end
 
 end
