112b9869a0dd9ea60ac82f39eaac54282f9a403a
Ivan_Ivanovich_Ivanov <antirsi2@yandex.ru>
2019-07-13 02:32:40 +0300

    flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,17 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user,
+                :logged_in?
 
   private
 
   def authenticate_user!
     unless current_user
-      redirect_to login_path
+      redirect_to login_path, alert: 'Неверная почта или пароль'
     end
+
+    cookies[:email] = current_user&.email
   end
 
   def current_user
