17f9da03f070361d2504a6fe3cba6fe74e3e64a4
Ivan_Ivanovich_Ivanov <antirsi2@yandex.ru>
2019-07-14 19:07:42 +0300

    fix2

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,15 +1,15 @@
 class TestsController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :set_test
 
   def index
     @tests = Test.all
   end
 
   def start
-    @test = Test.find(params[:id])
-    @user.tests.push(@test)
-    redirect_to @user.test_passage(@test)
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
   end
 
   private
@@ -20,10 +20,6 @@ class TestsController < ApplicationController
 
   def set_test
     @test = Test.find(params[:id])
-  end
-
-  def set_user
-    @user = User.first
   end
 
 end
